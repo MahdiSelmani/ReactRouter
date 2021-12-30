84ec0d5e43528a198c0cec18bf379f120bda79f9
MahdiSelmani <92759993+MahdiSelmani@users.noreply.github.com>
2021-12-30 13:53:26 +0100

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 function MovieList({ setMovies, movies, rating, word }) {
     return (
         <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: '50px' }}>
-            {movies.find(el=> el.title.toUpperCase().includes(word.toUpperCase())) ? movies.filter(el => el.rate >= rating && el.title.toUpperCase().includes(word.toUpperCase())).map(el => <MovieCard el={el} key={el.id} movies={movies} setMovies={setMovies} />) : <h3 style={{fontFamily:'cursive', fontStyle:'italic', margin:'50px'}}>We did not found this movie :(  </h3>}
+            {movies.find(el=> el.title.toUpperCase().includes(word.toUpperCase())) ? movies.filter(el => el.rate >= rating && el.title.toUpperCase().includes(word.toUpperCase())).map(el => <MovieCard el={el} key={el.id} movies={movies} setMovies={setMovies} />) : <h3 style={{fontFamily:'cursive', fontStyle:'italic', margin:'50px'}}>No Result :(  </h3>}
         </div>
     )
 }
